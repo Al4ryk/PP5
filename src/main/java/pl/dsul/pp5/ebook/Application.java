e5d41b9d5912330b44763dda76027f96c7bf0875
s-301-13 <easd@dsad.pl>
2020-01-25 12:59:47 +0100

    first commit

---
+++ src/main/java/pl/dsul/pp5/ebook/Application.java
@@ -0,0 +1,11 @@
+package pl.dsul.pp5.ebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
